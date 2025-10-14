75d3b9fd1e72fced5131dc882c27decb77b1327d
Swatipatil <swatippatil1530@gmail.com>
2025-10-14 23:12:06 +0530

    11th OCT 2025

---
+++ src/ex_03_Literals_variables/Lab024_string_conversion.py
@@ -0,0 +1,14 @@
+age = "90"
+print(type(age))
+
+age1 = 90
+print(type(age1))
+
+# we have the builtin functions we can conveert the data type of value
+#str(),int(),float(),bool()
+
+print(int(age))
+print(type(int(age)))
+
+"""name = "swati"
+print(int(name))""" # we cant convert the string to int or other type
---
+++ src/ex_03_Literals_variables/Lab027_strings.py
@@ -0,0 +1,11 @@
+name= "This is a big line"
+print(name)
+name = name + str(1)
+print((name))
+
+#concatenation arround strings
+fst_name = "swati"
+last_name = "patil"
+Full_name = fst_name +" " +last_name
+print(Full_name)
+print(type(Full_name))
